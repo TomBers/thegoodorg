112f029c21b5cb006433308a58a6b477f821c15a
princeofdales <j@da1e.com>
2015-06-16 17:19:12 +0100

    update to include freeform search on front page

--- client/templates/search/search.js
+++ client/templates/search/search.js
@@ -34,3 +34,35 @@ Template.search.events({
     projectSearch.search(text);
   }, 200)
 });
+
+
+//-------------------
+
+Template.searchResult2.helpers({
+  getProjects: function() {
+
+    return projectSearch.getData({
+      transform: function(matchText, regExp) {
+        return matchText.replace(regExp, "$&")
+      },
+      sort: {isoScore: -1}
+    });
+  }
+
+  // isLoading: function() {
+  //   return projectSearch.getStatus().loading;
+  // }
+});
+
+
+Template.searchResult2.rendered = function() {
+
+  // projectSearch.search('');
+};
+
+Template.searchMenu2.events({
+  "keyup #search-box2": _.throttle(function(e) {
+    var text = $(e.target).val().trim();
+    projectSearch.search(text);
+  }, 200)
+});
