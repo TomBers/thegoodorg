635a69625b28f5a4e2c35eb0dbc00858e2391c29
Kisan Kansagra <kisankansagra@hotmail.com>
2015-07-16 01:52:05 +0100

    fix map update on project page

--- client/templates/profile/editProject.js
+++ client/templates/profile/editProject.js
@@ -13,3 +13,28 @@ Template.editProject2.helpers({
         return UserProfiles.findOne({"loginEmail":this.employees[0]});
       }
 });
+
+
+function mapUpdate(){
+console.log("exe");
+var loc = $('input[name="postcode"]').val();
+console.log(loc);
+	Meteor.call("getLatLngfromAddress", loc, function(error, result){
+			   if(error){
+			    console.log("error");
+				Session.set('LatLng', false);
+				console.log(error);
+			   }
+
+			   Session.set('LatLng', result);
+		});
+}
+
+Template.editProject2.events = {
+
+  'keyup input[name="postcode"]': _.throttle(function(e, template) {
+    console.log("call");
+	//setTimeout(function(){},2000);
+		mapUpdate();
+  }, 3000, {leading: false, trailing: true})
+}
